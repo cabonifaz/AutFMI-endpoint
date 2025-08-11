75ad6b3175a77c9a0b3553d284c7dccf0d3cedb3
Denis I. A. <dial1901110514@gmail.com>
2025-08-11 12:34:56 -0500

    Remove imports unused

--- src/main/java/org/app/autfmi/service/impl/PostulantService.java
+++ src/main/java/org/app/autfmi/service/impl/PostulantService.java
@@ -1,13 +1,11 @@
 package org.app.autfmi.service.impl;
 
-import com.microsoft.sqlserver.jdbc.SQLServerException;
 import lombok.RequiredArgsConstructor;
 import org.app.autfmi.model.dto.LinkTokenDTO;
 import org.app.autfmi.model.request.BaseRequest;
 import org.app.autfmi.model.request.TalentRequest;
 import org.app.autfmi.model.response.BaseResponse;
 import org.app.autfmi.repository.PostulantRepository;
-import org.app.autfmi.repository.TalentRepository;
 import org.app.autfmi.service.IPostulantService;
 import org.app.autfmi.util.Common;
 import org.app.autfmi.util.Constante;
@@ -18,7 +16,6 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class PostulantService implements IPostulantService {
     private final PostulantRepository postulantRepository;
-    private final TalentRepository talentRepository;
     private final JwtHelper jwt;
 
     @Override

6855d275ebd38c4b948561aa18ca5fc3236d2ae7
zero <himura.js@hotmail.com>
2025-10-21 09:03:47 -0500

    FTBO-1039: feat(requirement): add contract duration fields to RequirementDTO and update repository methods

--- src/main/java/org/app/autfmi/repository/RequirementRepository.java
+++ src/main/java/org/app/autfmi/repository/RequirementRepository.java
@@ -407,7 +407,9 @@ public class RequirementRepository {
                 .addValue("ID_ROL", baseRequest.getIdRol())
                 .addValue("USUARIO", baseRequest.getUsername())
                 .addValue("ID_FUNCIONALIDADES", baseRequest.getFuncionalidades())
-                .addValue("MODALIDAD_FACT", request.getIdModalidadFact());
+                .addValue("MODALIDAD_FACT", request.getIdModalidadFact())
+                .addValue("ID_DUR_CONTRATO", request.getIdDuracionContrato())
+                .addValue("DURACION_CONTRATO", request.getDuracionContrato());
 
         Map<String, Object> result = simpleJdbcCall.execute(params);
         List<Map<String, Object>> resultSet = (List<Map<String, Object>>) result.get("#result-set-1");
@@ -924,7 +926,9 @@ public class RequirementRepository {
                 lstRqVacantes,
                 lstRqTalents,
                 lstRqFiles,
-                lstContactos);
+                lstContactos,
+                (Integer) requerimiento.get("ID_DUR_CONTRATO"),
+                (BigDecimal) requerimiento.get("DURACION_CONTRATO"));
     }
 
     private static SQLServerDataTable loadTvpRequirementFiles(List<FileRequest> lstArchivos, Integer idEmpresa)

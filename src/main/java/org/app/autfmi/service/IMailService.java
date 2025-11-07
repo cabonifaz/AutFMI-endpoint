594a416b396b234d882325b2fb0c06391c6375a9
zero <himura.js@hotmail.com>
2025-11-06 23:45:22 -0500

    FTBO-1044: feat(mail): simplify IMailService interface by removing deprecated notification methods

--- src/main/java/org/app/autfmi/service/IMailService.java
+++ src/main/java/org/app/autfmi/service/IMailService.java
@@ -1,33 +1,10 @@
 package org.app.autfmi.service;
 
 import java.util.List;
-import java.util.Map;
-import org.app.autfmi.model.dto.RequirementDTO;
 import org.app.autfmi.model.report.RequirementReport;
-import org.app.autfmi.util.MailUtils;
 
 public interface IMailService {
 
-        public MailUtils mailUtils = new MailUtils();
-
-        void sendCreateRequirementNotification(
-                        String userName,
-                        RequirementDTO rDto,
-                        List<Map<String, Object>> vacantesMapped,
-                        List<Map<String, Object>> contactosMapList,
-                        List<Map<String, Object>> habilidadesMapped,
-                        List<Map<String, Object>> carrerasMapped,
-                        String correoEjecutor);
-
-        void sendUpdateRequirementNotification(String userName,
-                        RequirementDTO rDto,
-                        List<Map<String, Object>> vacantesMapped,
-                        List<Map<String, Object>> contactosMapList,
-                        List<Map<String, Object>> habilidadesMapped,
-                        List<Map<String, Object>> carrerasMapped,
-                        List<Map<String, Object>> postulanteList,
-                        String correoEjecutor);
-
-        void sendRequirementNotificationV2(RequirementReport report, String subject, List<String> toAddresses,
-                        List<String> ccAddresses, String action);
+	void sendRequirementNotificationV2(RequirementReport report, String subject, List<String> toAddresses,
+			List<String> ccAddresses, String action);
 }

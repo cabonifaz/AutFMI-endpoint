1eb2a78ef9b5ffed93e5f4939ee1cf590fa0cae1
zero <himura.js@hotmail.com>
2026-02-13 21:54:46 -0500

    fix: Validación de tipo formulario para correo

--- src/main/java/org/app/autfmi/util/MailUtils.java
+++ src/main/java/org/app/autfmi/util/MailUtils.java
@@ -85,9 +85,12 @@ public class MailUtils {
     }
 
     private static String replaceDataToHtmlBody(String cuerpoCorreo, GestorRqDTO gestor, List<String> talentos) {
+
+        var tipoFormulario = gestor.getTipoFormulario() != null ? gestor.getTipoFormulario() : "Ingreso";
+
         return cuerpoCorreo.replace("[GESTOR]", gestor.getNombres())
                 .replace("[CLIENTE]", gestor.getCliente())
-                .replace("[TIPO_FORMULARIO]", gestor.getTipoFormulario())
+                .replace("[TIPO_FORMULARIO]", tipoFormulario)
                 .replace("{{listaTalentos}}", String.join("\n", talentos));
     }
 
